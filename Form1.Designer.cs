da7ed049c57834c0c27587fa264ef026d12b9c70
ThNgn <n.t.luong@agile-t.com>
2024-07-12 16:45:48 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CrabGame2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bg = new System.Windows.Forms.PictureBox();
+            this.crab = new System.Windows.Forms.PictureBox();
+            this.monkey = new System.Windows.Forms.PictureBox();
+            this.kaki = new System.Windows.Forms.PictureBox();
+            this.lychee = new System.Windows.Forms.PictureBox();
+            this.lychee2 = new System.Windows.Forms.PictureBox();
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crab)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monkey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lychee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lychee2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bg
+            // 
+            this.bg.Image = ((System.Drawing.Image)(resources.GetObject("bg.Image")));
+            this.bg.Location = new System.Drawing.Point(0, 0);
+            this.bg.Name = "bg";
+            this.bg.Size = new System.Drawing.Size(783, 461);
+            this.bg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg.TabIndex = 0;
+            this.bg.TabStop = false;
+            // 
+            // crab
+            // 
+            this.crab.Image = ((System.Drawing.Image)(resources.GetObject("crab.Image")));
+            this.crab.Location = new System.Drawing.Point(308, 331);
+            this.crab.Name = "crab";
+            this.crab.Size = new System.Drawing.Size(147, 118);
+            this.crab.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.crab.TabIndex = 1;
+            this.crab.TabStop = false;
+            // 
+            // monkey
+            // 
+            this.monkey.Image = ((System.Drawing.Image)(resources.GetObject("monkey.Image")));
+            this.monkey.Location = new System.Drawing.Point(326, 0);
+            this.monkey.Name = "monkey";
+            this.monkey.Size = new System.Drawing.Size(129, 103);
+            this.monkey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.monkey.TabIndex = 2;
+            this.monkey.TabStop = false;
+            // 
+            // kaki
+            // 
+            this.kaki.Image = ((System.Drawing.Image)(resources.GetObject("kaki.Image")));
+            this.kaki.Location = new System.Drawing.Point(145, 66);
+            this.kaki.Name = "kaki";
+            this.kaki.Size = new System.Drawing.Size(65, 62);
+            this.kaki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kaki.TabIndex = 3;
+            this.kaki.TabStop = false;
+            // 
+            // lychee
+            // 
+            this.lychee.Image = ((System.Drawing.Image)(resources.GetObject("lychee.Image")));
+            this.lychee.Location = new System.Drawing.Point(343, 109);
+            this.lychee.Name = "lychee";
+            this.lychee.Size = new System.Drawing.Size(65, 62);
+            this.lychee.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lychee.TabIndex = 4;
+            this.lychee.TabStop = false;
+            // 
+            // lychee2
+            // 
+            this.lychee2.Image = ((System.Drawing.Image)(resources.GetObject("lychee2.Image")));
+            this.lychee2.Location = new System.Drawing.Point(597, 90);
+            this.lychee2.Name = "lychee2";
+            this.lychee2.Size = new System.Drawing.Size(65, 62);
+            this.lychee2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lychee2.TabIndex = 5;
+            this.lychee2.TabStop = false;
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.Font = new System.Drawing.Font("Meiryo UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelGameOver.Location = new System.Drawing.Point(294, 174);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(187, 81);
+            this.labelGameOver.TabIndex = 6;
+            this.labelGameOver.Text = "WIN";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelScore.Location = new System.Drawing.Point(13, 186);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(141, 35);
+            this.labelScore.TabIndex = 7;
+            this.labelScore.Text = "Points: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelGameOver);
+            this.Controls.Add(this.lychee2);
+            this.Controls.Add(this.lychee);
+            this.Controls.Add(this.kaki);
+            this.Controls.Add(this.monkey);
+            this.Controls.Add(this.crab);
+            this.Controls.Add(this.bg);
+            this.Name = "Form1";
+            this.Text = "CrabGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crab)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monkey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lychee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lychee2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bg;
+        private System.Windows.Forms.PictureBox crab;
+        private System.Windows.Forms.PictureBox monkey;
+        private System.Windows.Forms.PictureBox kaki;
+        private System.Windows.Forms.PictureBox lychee;
+        private System.Windows.Forms.PictureBox lychee2;
+        private System.Windows.Forms.Label labelGameOver;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CrabGame2
+{
+    public partial class Form1 : Form
+    {
+
+        //Constants
+        private const int ItemFallSpeed = 10;       //Speed at which items fall down the form 
+        private const int WinScore = 5;             //Score required to win the game 
+        private const int LoseScore = -3;           //Score that results in losing the game 
+
+        //Fields
+        private Random random = new Random();       //Random number generator for item positions
+        private int score = 0;                      //Current score of the player
+
+        //Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();           //Initialize game elenments and start conditions 
+
+        }
+
+        //Initialize game elements and start conditions
+        private void InitializeGame()
+        {
+            labelGameOver.Visible = false;      //Hide the game over message initially
+
+            //Set transparent background for game elements 
+            SetTransparentBackground(crab);
+            SetTransparentBackground(monkey);
+            SetTransparentBackground(kaki);
+            SetTransparentBackground(lychee);
+            SetTransparentBackground(lychee2);
+
+        }
+
+        //Sets a control's back color to transparent and assigns it to the background control
+        private void SetTransparentBackground(Control control)
+        {
+            control.Parent = bg;                        //Set control's parent to the background panel
+            control.BackColor = Color.Transparent;      //Set control's background color to transparent
+        }
+
+        //Timer tick event handler
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveItemDown();             //Move items(kaki, lychee, lychee2) down the form
+            CheckItemCollection();      //Check if the crab collects any items 
+            CheckGameOver();            //Check if the game is over based on score
+        }
+
+        //Move items(kaki, lychee, lychee2) down the form or reset their position if they fall off
+        private void MoveItemDown()
+        {
+            MoveItemDown(kaki);         //Move kaki down or reset its position
+            MoveItemDown(lychee);       //Move lychee down or reset its position
+            MoveItemDown(lychee2);      //Move lychee2 down or reset its position
+        }
+
+        //Move a single item down the form or reset its position if it falls off
+        private void MoveItemDown(Control item)
+        {
+            //If item has fallen off the bottom of the form
+            if (item.Top >= 500)
+            {
+                //Reset item's position to a random X- coordinate within the form's width
+                item.Location = new Point(random.Next(50, 700), 0);
+            }
+            else
+            {
+                //Move item down by ItemFallSpeed pixels
+                item.Top += ItemFallSpeed;
+            }
+        }
+
+        //Check if the crab collects any items (kaki, lychee, lychee2) and update score
+        private void CheckItemCollection()
+        {
+            //If crab collects kaki
+            if (CollectItem(kaki))
+            {
+                UpdateScore(1);                 //Increase score 
+                ResetItemPosition(kaki);        //Reset kaki's position
+            }
+
+            //If crab collects lychee
+            if (CollectItem(lychee))
+            {
+                UpdateScore(-1);                //Decrease score 
+                ResetItemPosition(lychee);      //Reset lychee's position
+            }
+
+            //If crab collects lychee2
+            if (CollectItem(lychee2))
+            {
+                UpdateScore(-1);                //Decrease score 
+                ResetItemPosition(lychee2);     //Reset lychee2's position
+            }
+        }
+
+        //Check if the crab intersects with a specific item and update score
+        private bool CollectItem(Control item)
+        {
+            //Check if crab insersects with any items 
+            if (crab.Bounds.IntersectsWith(item.Bounds))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Display the score and display it in the score labal
+        private void UpdateScore(int change)
+        {
+            score += change;                            //Update score by the secified change 
+            labelScore.Text = $"Points: {score}";       //Display updated score in the score label 
+
+            //If score reaches the win condition
+            if (score == WinScore)
+            {
+                EndGame("WIN");             //End the game with a win message 
+            }
+            //If score reaches the lose condition
+            else if (score == LoseScore)
+            {
+                EndGame("LOSE");            //End the game with a lose message 
+            }
+        }
+
+        //Reset the position of a specific item to a random location at the top of the form 
+        private void ResetItemPosition(Control item)
+        {
+            item.Location = new Point(random.Next(50, 700), 0);         //Set item's position to a random location at the top
+        }
+
+        //End the game with a specified message
+        private void EndGame(string message)
+        {
+            labelGameOver.Text = message;       //Display the game over message 
+            labelGameOver.Visible = true;       //Show the over label
+            timer1.Enabled = false;             //Stop the game timer
+        }
+
+        //Key down event handler for player movement
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            MoveCrab(e.KeyCode);        //Move the crab based on the pressed key
+        }
+
+        //Move the crab left or right based on the pressed key 
+        private void MoveCrab(Keys key)
+        {
+            const int CrabMoveDistance = 10;        //Distance to move the crab
+
+            if (key == Keys.Left)
+            {
+                crab.Left -= CrabMoveDistance;      //Move crab left
+            }
+            else if (key == Keys.Right)
+            {
+                crab.Left += CrabMoveDistance;      //Move crab right 
+            }
+        }
+
+        //Check if the game is over (score reaches win or lose conditions)
+        private void CheckGameOver()
+        {
+            if (score == WinScore || score == LoseScore)
+            {
+                EndGame(score == WinScore ? "WIN" : "LOSE");        //End the game with win or lose message 
+            }
+        }
+
+    }
+}
